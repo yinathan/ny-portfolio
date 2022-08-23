b09d1dcec2239997648261c488a3a9c7dc9656c8
Nathan Yi <nathanyi112@gmail.com>
2022-08-22 22:51:53 -0500

    adds css variables and edits header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <header>
+        <div className="container header__container">
+            <h5>Hello I'm</h5>
+            <h1>Nathan Yi</h1>
+            <h5 className='text-light'>FullStack Developer
+            </h5>
+            <CTA />
+
+
+        </div>
+    </header>
+    )
 }
 
 export default Header
